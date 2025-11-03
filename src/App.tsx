a4f8cd001ed455fac1f6f9d13089aa1ab53ead3d
Dylan S <dsand171@mtroyal.ca>
2025-11-02 21:12:59 -0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Sponsors } from "./components/sponsors/Sponsors";
 import { PastEvents } from "./components/events/PastEvents";
 import { Footer } from "./components/footer/Footer";
 import { About } from "./components/about/About";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
